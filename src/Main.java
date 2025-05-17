b9e5cad26005bf8cbb259b39bd9d4c771fb34ce5
sabyn2705 <trinhdihoc2705@gmail.com>
2025-05-17 16:13:43 +0700

    commit02:Hello Admin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello Vitamincode03-BunBunLmao!");
+        System.out.print("Hello Vitamincode03-DakDakBunBunLmaoLmao!");
+        System.out.println("Hello Admin");
     }
 }
