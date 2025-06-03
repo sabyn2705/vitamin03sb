08a7f1485a401f2be29202070e541627faae0ab3
sabyn2705 <trinhdihoc2705@gmail.com>
2025-06-03 23:17:29 +0700

    tinh hinh chu nhat

---
+++ java/devjava/src/hinhchunhat.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class hinhchunhat {
+    public static void main(String[] args) {
+        System.out.println("Nhap hinh chu nhat: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Chieu dai: ");
+        int width = sc.nextInt();
+        System.out.print("Chieu rong: ");
+        int height = sc.nextInt();
+        System.out.println("Chu Vi: =" + ((width+height)*2));
+        System.out.println("Dien Tich: =" + (width*height));
+    }
+}
